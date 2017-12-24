0397374615e0f23f9ce18f7cbf4b5ef06734ff6a
Andrew Tang <aytee17.github@outlook.com>
2017-12-24 22:51:38 +1000

    Update up.sh

--- up.sh
+++ up.sh
@@ -5,6 +5,7 @@
 
 function up() {
     re='^[0-9]+$'
+    # if the number of arguments is greater than one or if the first argument is provided and it is not an integer
     if [ $# -gt 1 ] || (! [[ -z "$1" ]] && ! [[ $1 =~ $re ]])
     then
         printf "usage: up [levels]\n  levels: the number of directories to go up\n"
