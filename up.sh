7ef9ecd4669a76403f8a98b8ca39406ae5caed67
Andrew Tang <aytee17.github@outlook.com>
2017-12-24 22:26:29 +1000

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,20 +1,19 @@
-#!/bin/bash                                                                     
-                                                                                
-# Include the following line to .bashrc                                         
-# . /path/to/up                                                                 
-                                                                                
-function up() {                                                                 
-    if [ $# -gt 1 ]                                                             
-    then                                                                        
+#!/bin/bash
+
+# Include the following line to .bashrc
+# . /path/to/up
+
+function up() {
+    if [ $# -gt 1 ]
+    then
         printf "usage: up [levels]\n  levels: the number of directories to go up\n"
-        return -1                                                               
-    fi                                                                          
-                                                                                
-    local count=${1-1}                                                          
-                                                                                
+        return -1
+    fi
+    
+    local count=${1-1}
     while [ $count -gt 0 ]
-    do                                                  
-       cd ..                                                                    
-       let "count--"                                                            
-    done                                                                        
-}    
+    do
+       cd ..
+       let "count--"
+    done
+}
