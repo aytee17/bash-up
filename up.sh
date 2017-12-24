e1001895b1d6c831d097b46f0102350a259b43e0
Andrew Tang <aytee17.github@outlook.com>
2017-12-24 22:18:41 +1000

    Create up.sh

---
+++ up.sh
@@ -0,0 +1,20 @@
+#!/bin/bash                                                                     
+                                                                                
+# Include the following line to .bashrc                                         
+# . /path/to/up                                                                 
+                                                                                
+function up() {                                                                 
+    if [ $# -gt 1 ]                                                             
+    then                                                                        
+        printf "usage: up [levels]\n  levels: the number of directories to go up\n"
+        return -1                                                               
+    fi                                                                          
+                                                                                
+    local count=${1-1}                                                          
+                                                                                
+    while [ $count -gt 0 ]
+    do                                                  
+       cd ..                                                                    
+       let "count--"                                                            
+    done                                                                        
+}    
